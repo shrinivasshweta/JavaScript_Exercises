be253b9f332ed4d2c0cfa7967d67a603a41e5f79
SHWETA <shrinivasshweta@gmail.com>
2018-07-23 11:22:41 +0530

    Variable exercise 1

---
+++ createvar.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p> Create a variable called carName, assign the value "Volvo" to it, and display it.</p>
+<p id="demo">Display the result here.</p> 
+
+<script>
+var carName = "Volvo";
+document.getElementById("demo").innerHTML = carName;
+</script>
+
+</body>
+</html>
