3896b68cff607f2a3e840773c79d70c255fab648
SHWETA <shrinivasshweta@gmail.com>
2018-07-23 11:34:57 +0530

    Variable Exercise 3

---
+++ Sum.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p> Display the sum of 5 + 10, using two variables x and y. </p> 
+<p id="demo">Display the result here.</p> 
+
+<script>
+var x = 5;
+var y = 10;
+document.getElementById("demo").innerHTML = x + y;
+</script>
+
+</body>
+</html>
