abc7e5d558d17819c5b3b69b54a94c863f1baffe
SHWETA <shrinivasshweta@gmail.com>
2018-07-23 11:49:12 +0530

    Object Exercise 2

---
+++ AddPropertyValue.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Add the following property and value to the person object: country: USA</p>
+<p id="demo"></p>
+
+<script>
+var person = {firstName:"John", lastName:"Doe",country:"USA"};
+document.getElementById("demo").innerHTML = person.country;
+</script>
+
+</body>
+</html>
