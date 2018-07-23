0efd2425618648056ca9730f289aa41be4fb93d2
SHWETA <shrinivasshweta@gmail.com>
2018-07-23 11:41:00 +0530

    Variable Exercise 5

---
+++ singleVar.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>
+Use a single var keyword to create three variables with the following values: 
+firstName = "John" 
+lastName = "Doe" 
+age = 35
+
+</p>
+<p id="demo">Display the result here.</p> 
+
+<script>
+var firstName="John", lastName="Doe",age=35;
+
+document.getElementById("demo").innerHTML = 
+firstName + " " + lastName + " is " + age;
+</script>
+
+</body>
+</html>
